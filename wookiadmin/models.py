f678df3dea7d0e89278699d8c3b2de43807b4816
Athul Muralidhar <athul.m68@gmail.com>
2021-03-21 15:25:16 +0530

    ADD: added shell_plus for debugging
    ADD: added initial migrations for wookiadmin app
    
    WIP: working on field validations

--- wookiadmin/models.py
+++ wookiadmin/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import ValidationError 
 
 
 class Continent(models.Model):
-    class ContinentName(models.TextChoice):
+    class ContinentName(models.TextChoices):
         NORTHAMERICA = 'NA', _('North America')
         SOUTHAMERICA = 'SA', _('South America')
         ANTARTICA = 'AN', _('Antartica')
@@ -12,12 +13,27 @@ class Continent(models.Model):
         AFRICA = 'AF', _('Africa') 
         AUSTRALIA = 'AU', _('Australia')
     
-    name = models.Charfield(max_length=2, choices=ContinentName.choices, default=ContinentName.EUROPE)
+    name = models.CharField(max_length=2, choices=ContinentName.choices, default=ContinentName.EUROPE)
     population = models.PositiveBigIntegerField()
     area = models.PositiveBigIntegerField()
 
+    def __str__(self):
+        return self.get_name_display()
+
+    def clean_population(self):
+        passed_population = self.cleaned_data.get("population")
+        print(passed_population)
+        aggregated_population = aelf.country_set.all().aggregate(models.Sum('population'))
+        print(aggregated_population)
+
+        if passed_population > aggregated_population:
+            raise ValidationError('The passed population is greater than the sum of the popluaitons of all the counties in the continent')
+
+
+    
+
 class Country(models.Model):
-    name = models.Charfield(max_length=200)
+    name = models.CharField(max_length=200)
     population = models.PositiveBigIntegerField()
     area = models.PositiveBigIntegerField()
 
@@ -29,9 +45,15 @@ class Country(models.Model):
 
     continent = models.ForeignKey('Continent', on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name_plural = 'Countries'
+
+    def __str__(self):
+        return self.name
+
 
 class City(models.Model):
-    name = models.Charfield(max_length=200)
+    name = models.CharField(max_length=200)
     population = models.PositiveBigIntegerField()
     area = models.PositiveBigIntegerField()
 
@@ -41,3 +63,9 @@ class City(models.Model):
     number_of_shops = models.PositiveBigIntegerField()
 
     country = models.ForeignKey('Country', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = 'Cities'
+
+    def __str__(self):
+        return self.name
--- wookipedia/settings.py
+++ wookipedia/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_extensions',
+    'wookiadmin.apps.WookiadminConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
